c41ca4c1792e3fafda3b7df8b4c25fef3cacfb24
Sasha <sashapysmenny@gmail.com>
2020-12-11 17:07:47 +0200

    init

---
+++ lab3_1_oop/MyMatrix.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3_1_oop
+{
+    class MyMatrix
+    {
+        private double[,] matrix;
+
+        public MyMatrix(MyMatrix matrix_)
+        {
+            matrix = (double[,])matrix_.matrix.Clone();
+        }
+
+        public MyMatrix(double[,] arr)
+        {
+            matrix = (double[,])arr.Clone();
+        }
+
+        public MyMatrix(double[][] arr)
+        {
+            bool isRectangular = true;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i].Length != arr[0].Length)
+                {
+                    isRectangular = false;
+                }
+            }
+
+            if (isRectangular)
+            {
+                matrix = new double[arr.Length, arr[0].Length];
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = arr[i][j];
+                    }
+                }
+            }
+        }
+
+        public MyMatrix(string[] arr)
+        {
+            char[] separator = { '\t', ' ' };
+            string[] tmp = arr[0].Trim().Split(separator);
+            int rowCount = arr.GetLength(0);
+            int columnCount = tmp.Length;
+            matrix = new double[rowCount, columnCount];
+            for (int i = 0; i < Height; i++)
+            {
+                tmp = arr[i].Trim().Split(separator);
+                if (columnCount == tmp.Length)
+                {
+                    for (int j = 0; j < Width; j++)
+                    {
+                        matrix[i, j] = Int32.Parse(tmp[j]);
+                    }
+                }
+            }
+        }
+
+        public MyMatrix(string matrixInString) : this(matrixInString.Trim().Split('\n'))
+        { }
+
+        public static MyMatrix operator +(MyMatrix matrix_1, MyMatrix matrix_2)
+        {
+            MyMatrix sumOfMatrix;
+            if (matrix_1.Height == matrix_2.Height && matrix_1.Width == matrix_2.Width)
+            {
+                sumOfMatrix = new MyMatrix(matrix_1);
+                for (int i = 0; i < sumOfMatrix.Height; i++)
+                {
+                    for (int j = 0; j < sumOfMatrix.Width; j++)
+                    {
+                        sumOfMatrix.matrix[i, j] += matrix_2.matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("These matrix are not same size!!!");
+            }
+            return sumOfMatrix;
+        }
+
+        public static MyMatrix operator *(MyMatrix matrix_1, MyMatrix matrix_2)
+        {
+            MyMatrix mulMatrix;
+            if (matrix_1.Width == matrix_2.Height)
+            {
+                mulMatrix = new MyMatrix(new double[matrix_1.Height, matrix_2.Width]);
+                for (int i = 0; i < matrix_1.Height; i++)
+                {
+                    for (int j = 0; j < matrix_2.Width; j++)
+                    {
+                        for (int k = 0; k < matrix_2.Height; k++)
+                        {
+                            mulMatrix.matrix[i, j] += matrix_1.matrix[i, k] * matrix_2.matrix[k, j];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Column count first matrix isn't equal to row count second matrix.");
+            }
+            return mulMatrix;
+        }
+
+        public int Height
+        {
+            get
+            {
+                return matrix.GetLength(0);
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return matrix.GetLength(1);
+            }
+        }
+
+        public int GetWidth()
+        {
+            return matrix.GetLength(1);
+        }
+
+        public int GetHeight()
+        {
+            return matrix.GetLength(0);
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return matrix[i, j];
+            }
+            set
+            {
+                if (i < Height && j < Width && i >= 0 && j >= 0)
+                {
+                    matrix[i, j] = value;
+                }
+            }
+        }
+
+        public double GetElement(int i, int j)
+        {
+            return matrix[i, j];
+        }
+
+        public void SetElement(int i, int j, double val)
+        {
+            if (i < Height && j < Width && i >= 0 && j >= 0)
+            {
+                matrix[i, j] = val;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    str.Append(matrix[i, j] + "\t");
+                }
+                if (i != matrix.GetLength(0) - 1)
+                {
+                    str.Append("\n");
+                }
+            }
+            return str.ToString();
+        }
+
+        private double[,] GetTransponedArray()
+        {
+            double[,] transponedMatrix = new double[Width, Height];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    transponedMatrix[j, i] = matrix[i, j];
+                }
+            }
+            return transponedMatrix;
+        }
+
+        public MyMatrix GetTransponedCopy()
+        {
+            return new MyMatrix(GetTransponedArray());
+        }
+
+        public void TransponedMe()
+        {
+            matrix = GetTransponedArray();
+        }
+    }
+}
---
+++ lab3_1_oop/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace lab3_1_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m =  ");
+            int m = int.Parse(Console.ReadLine());
+            double[,] arrayMatrix = FillingMatrix(n, m);
+            double[,] arrayMatrix2 = FillingMatrix(n, m);
+            MyMatrix m1 = new MyMatrix(arrayMatrix);
+            MyMatrix m2 = new MyMatrix(arrayMatrix2);    
+            string[] stringArray = {"1 2 3 4",
+                                    "5 3 15 0",
+                                    "9 1 5 -1"};
+
+            //// створення примірника класу MyMatrix за допомогою двовимірного масиву масива
+            MyMatrix matrixFrom2DArray = new MyMatrix(arrayMatrix);
+            Console.WriteLine("Матриця з двовимiрного масиву:\n");
+            Console.WriteLine(matrixFrom2DArray);
+
+            //// створення примірника класу MyMatrix за допомогою уже створеного примірника цього ж класу
+            MyMatrix matrixFromMyMatrix = new MyMatrix(matrixFrom2DArray);
+            Console.WriteLine("\nМатриця з примiрника класу MyMatrix:\n");
+            Console.WriteLine(matrixFromMyMatrix);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// створення нового примірника класу MyMatrix за допомогою масиву рядків
+            MyMatrix matrixFromStringArray = new MyMatrix(stringArray);
+            Console.WriteLine("\nМатриця з масиву рядкiв:\n");
+            Console.WriteLine(matrixFromStringArray);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+
+            //// створення нового примірника з рядка
+            MyMatrix matrixFromString = new MyMatrix(matrixFrom2DArray.ToString());
+            Console.WriteLine("\nМатриця з рядка (який утворений з двовимiрного масиву):\n");
+            Console.WriteLine(matrixFromString);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// створення нового примірника класу MyMatrix за допомогою зубчатого масива
+            double[][] juggedArray = FillingJuggedArray(m, n);
+            MyMatrix matrixFromJuggedArray = new MyMatrix(juggedArray);
+            Console.WriteLine("\nМатриця з зубчатого масиву (заповненого випадковими числами):\n");
+            Console.WriteLine(matrixFromJuggedArray);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// сума матриць
+            Console.WriteLine("\nСума матриць:\n");
+            Console.WriteLine("Перша матриця: \n" + matrixFrom2DArray + "\n");
+            Console.WriteLine("Друга матриця: \n" + matrixFromMyMatrix + "\n");
+            MyMatrix sumMatrix = matrixFrom2DArray + matrixFromMyMatrix;
+            Console.WriteLine("\nРезультат:\n" + sumMatrix);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// множення матриць
+            Console.WriteLine("\nМноження матриць:\n");
+            Console.WriteLine("Перша матриця: \n" + matrixFrom2DArray + "\n");
+            Console.WriteLine("Друга матриця: \n" + matrixFromJuggedArray + "\n");
+            MyMatrix mulMatrix = matrixFrom2DArray * matrixFromJuggedArray;
+            Console.WriteLine("\nРезультат:\n" + mulMatrix);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// індексатори
+            Console.WriteLine("\nIндексатор:\n");
+            Console.WriteLine("\nМатриця до змiн:\n");
+            Console.WriteLine(matrixFrom2DArray);
+            matrixFrom2DArray[0, 0] = 777;
+            Console.WriteLine("Змiнено arr[0, 0] = " + matrixFrom2DArray[0, 0]);
+            Console.WriteLine("\nМатриця пiсля змiн:\n");
+            Console.WriteLine(matrixFrom2DArray);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// доступ і зміна елемента за допомогою методів Java-style
+            Console.WriteLine("\nJava-style доступ i замiна елемента:\n");
+            Console.WriteLine("\nМатриця до змiн:\n");
+            Console.WriteLine(matrixFrom2DArray);
+            matrixFrom2DArray.SetElement(0, 0, 666);
+            Console.WriteLine("Змiнено arr[0, 0] = " + matrixFrom2DArray.GetElement(0, 0));
+            Console.WriteLine("\nМатриця пiсля змiн:\n");
+            Console.WriteLine(matrixFrom2DArray);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// транспонування матриці зі створенням копії
+            Console.WriteLine("\nМатриця до транспонування:\n");
+            Console.WriteLine(matrixFromStringArray);
+            MyMatrix transponeMatrix = matrixFromStringArray.GetTransponedCopy();
+            Console.WriteLine("\nНова транспонована матриця:\n");
+            Console.WriteLine(transponeMatrix);
+            Console.WriteLine("\nМатриця пiсля транспонування:\n");
+            Console.WriteLine(matrixFromStringArray);
+            Console.Write("\nPress any key to continue ...\n");
+            Console.ReadKey();
+
+            //// транспонування матриці для якої викликається метод
+            Console.WriteLine("\nМатриця до транспонування:\n");
+            Console.WriteLine(matrixFromMyMatrix);
+            matrixFromMyMatrix.TransponedMe();
+            Console.WriteLine("\nМатриця пiсля транспонування:\n");
+            Console.WriteLine(matrixFromMyMatrix);
+            Console.Write("\nPress any key to continue ...\n");
+
+            Console.ReadKey();
+        }
+
+        static double[,] FillingMatrix(int n, int m)
+        {
+            double[,] matrix = new double[n, m];
+            Random rand = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rand.Next(0, 15);
+                }
+            }
+            return matrix;
+        }
+
+        static double[][] FillingJuggedArray(int n, int m)
+        {
+            double[][] arr = new double[n][];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new double[m];
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] = rand.Next(0, 15);
+                }
+            }
+            return arr;
+        }
+    }
+}
